c57f087a4717d3a7aaa8e9e237e20434be237343
Verginia Stolear <Verginia.Stolear@gmail.com>
2023-06-13 17:41:15 +0300

    Implement the Driver Manager and configure the Selenium dependencies

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,7 +1,51 @@
 package com.opencart;
 
+
+import com.opencart.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 public class TestRunner {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //        DriverManager driverManager = DriverManager.getInstance();
+        //        WebDriver driver1 = driverManager.getDriver();
+
+        // Define a driver object that will be used for future actions.
+        WebDriver driver = DriverManager.getInstance().getDriver();
+
+        driver.get("https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox");
+
+        String currentWindowName = driver.getWindowHandle();
+
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        driver.get("https://www.youtube.com/");
+        driver.close();
+
+        driver.switchTo().window(currentWindowName);
+        driver.get("https://protv.md/");
+        driver.quit();
+
+        System.out.println("The driver is closed");
     }
 }
+
+
+
+
+/*import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestRunner {
+    public static void main(String[] args) throws InterruptedException {
+      //  WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com/");
+        Thread.sleep(3000);
+
+        driver.quit();
+    }
+}
+*/
---
+++ src/main/java/com/opencart/managers/DriverManager.java
@@ -0,0 +1,58 @@
+package com.opencart.managers;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class DriverManager {
+    private static String webDriverType = "Chrome";
+    private static DriverManager instance;
+    private WebDriver driver;
+
+    private DriverManager() {
+        switch (webDriverType.toUpperCase()) {
+            case "CHROME":
+             //   WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                System.out.println("The Chrome Driver was initiated!");
+                break;
+            case "FIREFOX":
+            //    WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                System.out.println("The FireFox Driver was initiated!");
+                break;
+            case "EDGE":
+           //     WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                System.out.println("The Edge Driver was initiated!");
+                break;
+            case "SAFARI":
+            //    WebDriverManager.safaridriver().setup();
+                driver = new SafariDriver();
+                System.out.println("The Safari Driver was initiated!");
+                break;
+            default:
+                System.out.println("There is not defined such a driver:" + webDriverType);
+        }
+    }
+
+    // Metoda statica pentru a obtine instanta Singleton
+    public static DriverManager getInstance() {
+        if (instance == null) {
+            instance = new DriverManager();
+        }
+        return instance;
+    }
+
+    // Metoda publica pentru a obtine driverul web
+    public WebDriver getDriver() {
+        if (driver == null) {
+            // getInstance();
+            DriverManager.getInstance();
+        }
+        return driver;
+    }
+}
